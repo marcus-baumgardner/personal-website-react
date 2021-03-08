d48b776f95697a70154170f0eba582dc3dbeac61
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2021-03-08 15:47:14 -0600

    moved from nodemailer to emailerjs until I can get nodemailer fixed. emailjs is successfully sending emails to my email address based on the form. The page dynamically displays a success and a failure based on the response from emailerjs

--- frontend/src/Components/ContactForm/ContactForm.jsx
+++ frontend/src/Components/ContactForm/ContactForm.jsx
@@ -1,64 +1,75 @@
 import React, { useState } from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
-import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
+
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
 
-    const [ values, setValues ] = useState({});
+    const [ status, setStatus ] = useState(null);
+    const [ response, setResponse ] = useState(null);
 
-    const sendEmail = () => {
+    const sendEmail = (e) => {
         
-        const url = "http://localhost:5000/api/send"
+        e.preventDefault();
 
-        const email = {
-            name: values.name,
-            email: values.email,
-            message: values.message
+        emailjs.sendForm('service_r9s0kji', 'template_v7c0h8j', e.target, 'user_URr9hAdzBvAn0dPzQZyux')
+        .then((result) => {
+            console.log(result.text);
+            setStatus('success')
+            setResponse(result.text);
+        }, (error) => {
+            console.log(error.text);
+            setStatus('failure')
+            setResponse(error.text);
+        });
+        e.target.reset();
+    }
+
+
+    const result = () => {
+        if (status === 'success') {
+            return(
+                <h1>SUCCESS! {response} </h1>
+            )
+        } else if (status === 'failure') {
+            return(
+                <h1>Failure! {response} </h1>
+            )
         }
-
-        console.log("email: ", email);
-
-        axios.post(url, email)
-        .then(res => {
-            console.log(res);
-        })
-        .catch(err => console.log(err));
-
     }
 
-    const handleChange = (event) => {
-        event.persist();
-        setValues(values => ({...values, [event.target.name]: event.target.value}))
-        console.log(values);
-    }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(values);
-        sendEmail();
-    }
 
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>Name: </Form.Label>
-                
-                <Form.Control type="text" placeholder="Enter Full Name" name="name" value={values.name} onChange={handleChange} />
-            </Form.Group>
+        
+        <React.Fragment>
+            <Form onSubmit={sendEmail}>
+                <Form.Group>
+                    <Form.Label>Name: </Form.Label>
+                    
+                    <Form.Control type="text" placeholder="Enter Full Name" name="name" />
+                </Form.Group>
 
-            <Form.Group>   
-                <Form.Label>Email: </Form.Label>
-                <Form.Control type="email" placeholder="Enter Email" name="email" value={values.email} onChange={handleChange} />
-            </Form.Group>
+                <Form.Group>   
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control type="email" placeholder="Enter Email" name="email"  />
+                </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Message: </Form.Label>
-                <br/>
-                <Form.Control type="text" as="textarea" rows = {4} name="message" value={values.message} onChange={handleChange} />
-            </Form.Group>
-            <Button type="submit">Submit Message</Button>
-        </Form>
+                <Form.Group>
+                    <Form.Label>Message: </Form.Label>
+                    <br/>
+                    <Form.Control type="text" as="textarea" rows = {4} name="message" />
+                </Form.Group>
+                <Button type="submit">Submit Message</Button>
+            </Form>
+
+            <div>
+                {(status === 'success')  ? result() : null }
+            </div>
+
+        </React.Fragment>
+
     )
 }
 
